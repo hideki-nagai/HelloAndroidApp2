20c6274318c7e3089cf3856082f09f24b4dc5787
Hideki_Nagai <gaisan925@gmail.com>
2021-07-31 15:36:24 +0900

    最初のコミット（２回目）

---
+++ app/src/main/java/jp/techacademy/hideki/nagai/MainActivity.kt
@@ -0,0 +1,11 @@
+package jp.techacademy.hideki.nagai
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
